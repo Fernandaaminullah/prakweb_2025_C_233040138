5da66e9ce8da5d46ac1aa56db1770854ebed3f6e
Fernandaaminullah <147626548+Fernandaaminullah@users.noreply.github.com>
2025-11-12 18:51:38 +0700

    Add files via upload

---
+++ pertemuan2/Latihan/TemplateMVC/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("http://localhost/prakweb2025_C_233040138/Pertemuan2/Latihan/MVC/TemplateMVC-main/TemplateMVC-main/public/");
+
+// DATANASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2_pertemua2');
+
